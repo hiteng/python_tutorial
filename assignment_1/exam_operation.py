04aa88863f1658ea54b4bc4a5258fba4071e2584
Hitendra Gullapalli <hitendra@192-168-1-7.tpgi.com.au>
2018-09-10 13:44:08 +1000

    assignment_1 completed

---
+++ assignment_1/exam_operation.py
@@ -0,0 +1,66 @@
+
+
+
+
+from utils import perc_cal
+
+def exam_result(total_marks, out_of_marks):
+
+    if total_marks > out_of_marks:
+        result = "Invalid marks"
+        return result
+    total_perc = perc_cal(total_marks,out_of_marks)
+    print "total perc", total_perc
+
+    if total_perc >= 75:
+        result = "Student is passed with Distinction"
+    elif total_perc >= 50:
+        result = "Student is passed with First class"
+    elif total_perc >= 40:
+        result = "Student is passed"
+    else:
+        result = "Student is failed"
+    return result
+
+print "exam operation __name__ ", __name__
+print exam_result(430,500)
+
+def exam_result1(sub1, sub2, sub3, sub4, sub5, out_of_marks):
+
+    total_marks = sub1+sub2+sub3+sub4+sub5
+
+    if total_marks > out_of_marks:
+        result = "Invalid marks"
+        return result
+
+    if total_marks >= 375:
+        result = "Student is passed with Distinction"
+    elif total_marks >= 250:
+        result = "Student is passed with First class"
+    elif total_marks >= 200:
+        result = "Student is passed"
+    else:
+        result = "Student is failed"
+    return result
+
+print exam_result1(75,65,90,67,78,500)
+
+# def exam_result2(sub1, sub2, sub3, sub4, sub5, total_perc):
+#
+#     avg = (sub1+sub2+sub3+sub4+sub5)/5
+#
+#     if avg > total_perc:
+#         result = "Invalid Percentage"
+#         return result
+#
+#     if avg >= 75:
+#         result = "Student passed with distinction"
+#     elif avg >= 50:
+#         result = "Student is passed with First class"
+#     elif avg >= 40:
+#         result = "Student is passed"
+#     else:
+#         result = "Student is failed"
+#     return result
+#
+# print exam_result2(75,65,90,67,78,100)
